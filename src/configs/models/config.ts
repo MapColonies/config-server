9673400b26f7a0ce0d5ffb28ffc2d86e749328a7
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2024-05-16 07:45:47 +0300

    feat: created config ref connecting table

--- src/configs/models/config.ts
+++ src/configs/models/config.ts
@@ -1,4 +1,4 @@
-import { integer, jsonb, pgSchema, text, timestamp, primaryKey } from 'drizzle-orm/pg-core';
+import { integer, jsonb, pgSchema, text, timestamp, primaryKey, foreignKey, index } from 'drizzle-orm/pg-core';
 
 export const pgDbSchema = pgSchema('config_server');
 
@@ -16,6 +16,31 @@ export const configs = pgDbSchema.table(
     pk: primaryKey({ columns: [table.configName, table.version] }),
   })
 );
+
+export const configsRefs = pgDbSchema.table(
+  'config_refs',
+  {
+    configName: text('name').notNull(),
+    version: integer('version').notNull(),
+    refConfigName: text('ref_name').notNull(),
+    refVersion: integer('ref_version'),
+  },
+  (table) => ({
+    originalConfigRef: foreignKey({
+      columns: [table.configName, table.version],
+      foreignColumns: [configs.configName, configs.version],
+      name : 'config_refs_original_config_fk',
+    }),
+    childConfigRef: foreignKey({
+      columns: [table.refConfigName, table.refVersion],
+      foreignColumns: [configs.configName, configs.version],
+      name : 'config_refs_child_config_fk',
+    }),
+    index: index("idx_config_name_version").on(table.configName, table.version),
+  })
+);
+
+
 
 export type Config = typeof configs.$inferSelect;
 export type NewConfig = typeof configs.$inferInsert;

893cc1493fcb94578afab0f755bbbf91fd518596
Ofer <12687466+CptSchnitz@users.noreply.github.com>
2024-07-21 08:15:34 +0300

    refactor: changed to typedhandler (#38)

--- src/capabilities/controllers/capabilitiesController.ts
+++ src/capabilities/controllers/capabilitiesController.ts
@@ -1,12 +1,9 @@
 import { Logger } from '@map-colonies/js-logger';
-import { RequestHandler } from 'express';
 import httpStatus from 'http-status-codes';
 import { injectable, inject } from 'tsyringe';
 import { SERVICES } from '../../common/constants';
 import { CapabilitiesManager } from '../models/capabilitiesManager';
-import { paths } from '../../openapiTypes';
-
-type GetCapabilities = RequestHandler<undefined, paths['/capabilities']['get']['responses']['200']['content']['application/json']>;
+import { TypedRequestHandler } from '../../common/interfaces';
 
 @injectable()
 export class CapabilitiesController {
@@ -15,7 +12,7 @@ export class CapabilitiesController {
     @inject(CapabilitiesManager) private readonly manager: CapabilitiesManager
   ) {}
 
-  public getCapabilities: GetCapabilities = (req, res) => {
+  public getCapabilities: TypedRequestHandler<'/capabilities', 'get'> = (req, res) => {
     const capabilities = this.manager.getCapabilities();
     return res.status(httpStatus.OK).json(capabilities);
   };
--- src/schemas/controllers/schemaController.ts
+++ src/schemas/controllers/schemaController.ts
@@ -1,23 +1,11 @@
 import { Logger } from '@map-colonies/js-logger';
-import { RequestHandler } from 'express';
 import httpStatus from 'http-status-codes';
 import { injectable, inject } from 'tsyringe';
 import { HttpError } from '@map-colonies/error-express-handler';
 import { SERVICES } from '../../common/constants';
 import { SchemaManager } from '../models/schemaManager';
-import { paths } from '../../openapiTypes';
 import { SchemaNotFoundError, SchemaPathIsInvalidError } from '../models/errors';
-
-type GetSchemaTypes = paths['/schema']['get'];
-type GetSchema = RequestHandler<
-  undefined,
-  GetSchemaTypes['responses']['200']['content']['application/json'],
-  undefined,
-  GetSchemaTypes['parameters']['query']
->;
-
-type GetSchemasTypes = paths['/schema/tree']['get'];
-type GetSchemas = RequestHandler<undefined, GetSchemasTypes['responses']['200']['content']['application/json']>;
+import { TypedRequestHandler } from '../../common/interfaces';
 
 @injectable()
 export class SchemaController {
@@ -26,11 +14,11 @@ export class SchemaController {
     @inject(SchemaManager) private readonly manager: SchemaManager
   ) {}
 
-  public getSchema: GetSchema = async (req, res, next) => {
+  public getSchema: TypedRequestHandler<'/schema', 'get'> = async (req, res, next) => {
     try {
       const schema = await this.manager.getSchema(req.query.id, req.query.shouldDereference);
 
-      return res.status(httpStatus.OK).json(schema as unknown as GetSchemaTypes['responses']['200']['content']['application/json']);
+      return res.status(httpStatus.OK).json(schema as unknown as Record<string, never>);
     } catch (error) {
       if (error instanceof SchemaNotFoundError) {
         (error as HttpError).status = httpStatus.NOT_FOUND;
@@ -41,7 +29,7 @@ export class SchemaController {
     }
   };
 
-  public getSchemasTree: GetSchemas = async (req, res) => {
+  public getSchemasTree: TypedRequestHandler<'/schema/tree', 'get'> = async (req, res) => {
     const schemasTree = await this.manager.getSchemas();
     return res.status(httpStatus.OK).json(schemasTree);
   };

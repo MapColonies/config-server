d073dfec8539fed2f883d5e543775f0b78fce9fd
eyalr1100 <eyalr1100@gmail.com>
2026-01-27 10:44:09 +0200

    fix: make timeout configureable

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,12 @@
 import { readPackageJsonSync } from '@map-colonies/read-pkg';
+import config from 'config';
 
 const schemasPackagePath = require.resolve('@map-colonies/schemas').substring(0, require.resolve('@map-colonies/schemas').indexOf('build'));
 export const schemasPackageVersion = readPackageJsonSync(schemasPackagePath + 'package.json').version as string;
 
 export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
 export const DEFAULT_SERVER_PORT = 80;
-export const DB_CONNECTION_TIMEOUT = 5000;
+export const DB_CONNECTION_TIMEOUT = config.get<number>('db.connectionTimeout');
 
 export const MILLISECONDS_IN_SECOND = 1000;
 
